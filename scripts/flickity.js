c2eb69030c2a50b703bbc86ed5630fd9c79ed9ce
Bram <bram_vos@hotmail.com>
2020-06-04 18:05:36 +0200

    Added team-up case

--- scripts/flickity.js
+++ scripts/flickity.js
@@ -8,15 +8,27 @@ var flkty = new Flickity(elem, {
     selectedAttraction: 0.01,
     friction: 0.15,
     pageDots: false,
-
-
+    prevNextButtons: false,
 });
 
-// var flkty = new Flickity('.carousel');
+
+// Custom previous and next buttons
+var previousButton = document.querySelector('.previous-button');
+previousButton.addEventListener('click', function () {
+    flkty.previous();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener('click', function () {
+    flkty.next();
+});
+
+
+// Adding classes to the project shown to change the background color
 flkty.on('change', function (index) {
 
     console.log('Flickity change ' + index);
-    
+
     let indexClass = 'index-' + index;
 
     let element = document.getElementsByClassName("main-carousel")[0];
@@ -25,4 +37,31 @@ flkty.on('change', function (index) {
     element.classList.remove("index-2");
 
     element.classList.add(indexClass);
+});
+
+
+// Project information
+// Set the info in the data attr
+var client = document.querySelector('.client');
+
+flkty.on( 'select', function() {
+  client.textContent = $( flkty.selectedElement ).attr('data-client');
+});
+
+var project = document.querySelector('.project');
+
+flkty.on( 'select', function() {
+  project.textContent = $( flkty.selectedElement ).attr('data-project');
+});
+
+var year = document.querySelector('.year');
+
+flkty.on( 'select', function() {
+  year.textContent = $( flkty.selectedElement ).attr('data-year');
+});
+
+var number = document.querySelector('.number');
+
+flkty.on( 'select', function() {
+  number.textContent = $( flkty.selectedElement ).attr('data-number');
 });
---
+++ scripts/scroll.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', (event) => {
+
+    setTimeout(function (){
+
+        document.getElementById('scrollTo').scrollIntoView({block: 'start', behavior: 'smooth'});
+      
+      }, 800); // How long do you want the delay to be
+    
+    
+});
