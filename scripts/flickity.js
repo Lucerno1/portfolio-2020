68c56483215ce6536e978e656c6164a90620018b
Bram <bram_vos@hotmail.com>
2020-06-06 20:02:03 +0200

    Fixed view project + custom scrollbar

--- scripts/flickity.js
+++ scripts/flickity.js
@@ -1,36 +1,32 @@
-window.addEventListener('load', (event) => {
-  var elem = document.querySelector('.main-carousel');
+window.addEventListener("load", (event) => {
+  var elem = document.querySelector(".main-carousel");
   var flkty = new Flickity(elem, {
-      // options
-      cellAlign: 'center',
-      contain: true,
-      wrapAround: true,
-      selectedAttraction: 0.01,
-      friction: 0.15,
-      pageDots: false,
-      prevNextButtons: false,
+    // options
+    cellAlign: "center",
+    contain: true,
+    wrapAround: true,
+    selectedAttraction: 0.01,
+    friction: 0.15,
+    pageDots: false,
+    prevNextButtons: false,
   });
 
-
-
-// Custom previous and next buttons
-var previousButton = document.querySelector('.previous-button');
-previousButton.addEventListener('click', function () {
+  // Custom previous and next buttons
+  var previousButton = document.querySelector(".previous-button");
+  previousButton.addEventListener("click", function () {
     flkty.previous();
-});
+  });
 
-var nextButton = document.querySelector('.next-button');
-nextButton.addEventListener('click', function () {
+  var nextButton = document.querySelector(".next-button");
+  nextButton.addEventListener("click", function () {
     flkty.next();
-});
+  });
 
+  // Adding classes to the project shown to change the background color
+  flkty.on("change", function (index) {
+    console.log("Flickity change " + index);
 
-// Adding classes to the project shown to change the background color
-flkty.on('change', function (index) {
-
-    console.log('Flickity change ' + index);
-
-    let indexClass = 'index-' + index;
+    let indexClass = "index-" + index;
 
     let element = document.getElementsByClassName("main-carousel")[0];
     element.classList.remove("index-0");
@@ -38,33 +34,38 @@ flkty.on('change', function (index) {
     element.classList.remove("index-2");
 
     element.classList.add(indexClass);
-});
+  });
 
+  // Project information
+  // Set the info in the data attr
 
-// Project information
-// Set the info in the data attr
+  var client = document.querySelector(".client");
 
-var client = document.querySelector('.client');
+  flkty.on("select", function () {
+    client.textContent = $(flkty.selectedElement).attr("data-client");
+  });
 
-flkty.on( 'select', function() {
-  client.textContent = $( flkty.selectedElement ).attr('data-client');
-});
+  var project = document.querySelector(".project");
 
-var project = document.querySelector('.project');
+  flkty.on("select", function () {
+    project.textContent = $(flkty.selectedElement).attr("data-project");
+  });
 
-flkty.on( 'select', function() {
-  project.textContent = $( flkty.selectedElement ).attr('data-project');
-});
+  var year = document.querySelector(".year");
 
-var year = document.querySelector('.year');
+  flkty.on("select", function () {
+    year.textContent = $(flkty.selectedElement).attr("data-year");
+  });
 
-flkty.on( 'select', function() {
-  year.textContent = $( flkty.selectedElement ).attr('data-year');
-});
+  var number = document.querySelector(".number");
 
-var number = document.querySelector('.number');
+  flkty.on("select", function () {
+    number.textContent = $(flkty.selectedElement).attr("data-number");
+  });
 
-flkty.on( 'select', function() {
-  number.textContent = $( flkty.selectedElement ).attr('data-number');
-});
+  var link = document.querySelector(".project-link");
+
+  flkty.on("select", function () {
+    link.href = $(flkty.selectedElement).attr("data-link");
+  });
 });
